0516c806c6e7c3deb4265611320a3e63930d42b1
animkr95 <animkr95@gmail.com>
2019-11-06 22:55:49 +0400

    Problem2

---
+++ Problem2/Problem2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+       public static void Main()
+        {
+            ArrayList myList = new ArrayList(5);
+            myList.Add(1);
+            myList.Add(3);
+            myList.Add(5);
+            myList.Add(7);
+            myList.Add(9);
+            foreach (int itm in myList)
+            {
+                Console.WriteLine(itm);
+            }
+            Console.WriteLine("\nInput number");
+            int required_number =Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("The ArrayList after Removing elements");
+            myList.RemoveAt(required_number);
+            foreach (int itm in myList)
+            {
+                Console.WriteLine(itm);
+            }
+           
+        }
+}
+}
