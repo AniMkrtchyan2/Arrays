2cf750f73378d2f7738486081ab61fbb35de050b
animkr95 <animkr95@gmail.com>
2019-11-09 01:39:12 +0400

    Problem3changedcode

--- Problem3/Problem3/Program.cs
+++ Problem3/Problem3/Program.cs
@@ -11,23 +11,26 @@ namespace Problem3
         static void Main()
         {
             int[,] matrix = { {1,2,3}, {4,5,6}, {7,8,9} };
-            int rowLength = matrix.GetLength(0);
-            int colLength = matrix.GetLength(1);
             Console.WriteLine("given square matrix");
-            Upper(matrix, rowLength,colLength);
-        }
-        static void Upper(int[,] matrix, int rowLength,int colLength)
-        {
-            int i, j;
-            for (i = 0; i < rowLength; i++)
+           int i, j;
+            for (i=0 ; i <matrix.GetLength(0); i++)
             {
-                for (j = 0; j <colLength; j++)
+                for (j=0 ; j <matrix.GetLength(1); j++)
                 {
                     Console.Write(string.Format("{0} ", matrix[i, j]));
                 }
                 Console.Write(Environment.NewLine);
                
             }
+            Upper(matrix);
+            Console.ReadKey();
+        }
+        static void Upper(int[,] matrix)
+        {
+            int rowLength = matrix.GetLength(0);
+            int colLength = matrix.GetLength(1);
+            int i, j;
+           
             Console.WriteLine("Upper triangular matrix: ");
 
             for (i = 0; i <rowLength; i++)
